5a42858759d9712280709a7dc6417a5c8e5ebbc4
erjaymodi <jay.quixom@gmail.com>
2015-12-21 16:17:29 +0530

    procfile added for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rest_boiler.wsgi --log-level=DEBUG
--- rest_boiler/wsgi.py
+++ rest_boiler/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rest_boiler.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rest_boiler.settings.local")
 
 application = get_wsgi_application()
